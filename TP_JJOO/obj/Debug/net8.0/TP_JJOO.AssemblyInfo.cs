4c6788d2577a8df05f7d095f0a39f482da58248a
Marcelo Dominguez <48039458@est.ort.edu.ar>
2024-08-20 19:25:21 -0300

    fin de index

--- TP_JJOO/obj/Debug/net8.0/TP_JJOO.AssemblyInfo.cs
+++ TP_JJOO/obj/Debug/net8.0/TP_JJOO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_JJOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eb372485a7bc004c2c4f25b4d3635ff2b2e7cf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d5b4638ccf497c1a03daad8a5113fef03fc2eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_JJOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_JJOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
